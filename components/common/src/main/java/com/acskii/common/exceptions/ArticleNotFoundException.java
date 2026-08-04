bafdd5b1de8e8d85f5a0830cd3a1765d45e30712
Andrew Sameh Adel <andrew.sameh.adel@gmail.com>
2026-08-04 15:28:56 +0300

    [fix] fix article exception message

--- components/common/src/main/java/com/acskii/common/exceptions/ArticleNotFoundException.java
+++ components/common/src/main/java/com/acskii/common/exceptions/ArticleNotFoundException.java
@@ -2,6 +2,6 @@ package com.acskii.common.exceptions;
 
 public class ArticleNotFoundException extends RuntimeException {
     public ArticleNotFoundException(Long id) {
-        super(String.format("Source of ID (%d) does not exist", id));
+        super(String.format("Article of ID (%d) does not exist", id));
     }
 }
