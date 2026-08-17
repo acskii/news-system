49db3abb222e452965c2d38a552d9ecc048b43db
Andrew Sameh Adel <andrew.sameh.adel@gmail.com>
2026-08-17 16:48:51 +0300

    [fix] allow common database url by splitting to its components

--- components/database/connection.py
+++ components/database/connection.py
@@ -4,9 +4,11 @@ from sqlalchemy.orm import scoped_session, sessionmaker, declarative_base
 
 DATABASE_USER = os.getenv("DB_USER")
 DATABASE_PASS = os.getenv("DB_PASS")
+DATABASE_HOST = os.getenv("DB_HOST", "localhost")
+DATABASE_PORT = os.getenv("DB_PORT", "5432")
+DATABASE_NAME = os.getenv("DB_NAME", "news")
 
-# TODO: Needs changing to accomodate connection string formatting alongside Spring applications 
-DATABASE_URL = f"postgresql+psycopg://{DATABASE_USER}:{DATABASE_PASS}@localhost:5432/news"
+DATABASE_URL = f"postgresql+psycopg://{DATABASE_USER}:{DATABASE_PASS}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
 
 engine = create_engine(DATABASE_URL)
 db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
